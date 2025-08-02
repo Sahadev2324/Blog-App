b9319312bda5d81e0d21337b82aed5a15056e07a
Sahadev Sahoo <sahadevsahoo2324@gmail.com>
2025-08-03 04:16:47 +0530

    Update blogapi.js

--- src/api/blogapi.js
+++ src/api/blogapi.js
@@ -1,15 +1,26 @@
 // src/api/blogApi.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+// ✅ Define base API instance
+const API = axios.create({
+  baseURL: 'http://localhost:3001', // ✅ JSON Server running here
+});
 
-export const getBlogs = () => axios.get(`${API_URL}/blogs`).then(res => res.data);
-export const getBlogById = (id) => axios.get(`${API_URL}/blogs/${id}`).then(res => res.data);
-export const createBlog = (blog) => axios.post(`${API_URL}/blogs`, blog);
-export const updateBlog = (id, blog) => axios.put(`${API_URL}/blogs/${id}`, blog);
-export const deleteBlog = (id) => axios.delete(`${API_URL}/blogs/${id}`);
+// -------------------- BLOGS --------------------
+export const getBlogs = () => API.get('/blogs').then(res => res.data);
+export const getBlogById = (id) => API.get(`/blogs/${id}`).then(res => res.data);
+export const createBlog = (blog) => API.post('/blogs', blog);
+export const updateBlog = (id, blog) => API.put(`/blogs/${id}`, blog);
+export const deleteBlog = (id) => API.delete(`/blogs/${id}`);
 
-export const getAuthors = () => axios.get(`${API_URL}/authors`).then(res => res.data);
-export const getCategories = () => axios.get(`${API_URL}/categories`).then(res => res.data);
-export const createAuthor = (author) => axios.post(`${API_URL}/authors`, author);
-export const createCategory = (category) => axios.post(`${API_URL}/categories`, category);
+export const getBlog = getBlogById; // optional alias
+
+// -------------------- AUTHORS --------------------
+export const getAuthors = () => API.get('/authors').then(res => res.data);
+export const createAuthor = (name) => API.post('/authors', { name });
+export const deleteAuthor = (id) => API.delete(`/authors/${id}`);
+
+// -------------------- CATEGORIES --------------------
+export const getCategories = () => API.get('/categories').then(res => res.data);
+export const createCategory = (name) => API.post('/categories', { name });
+export const deleteCategory = (id) => API.delete(`/categories/${id}`);
